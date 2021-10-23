f85238ddfd1ccaf5fbc5965a4a2d46c50c5c09bc
brunobraunbaer <soeren.goessel@outlook.com>
2021-10-23 23:57:00 +0200

    Added Winner

--- app/src/main/java/com/pigo/guess_reloaded/Winner.java
+++ app/src/main/java/com/pigo/guess_reloaded/Winner.java
@@ -1,6 +1,8 @@
 package com.pigo.guess_reloaded;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +13,13 @@ public class Winner extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_winner);
+
+        Button btnWinner = (findViewById(R.id.btnRestart));
+
+        btnWinner.setOnClickListener(v -> {
+            Intent i = new Intent(Winner.this, Difficulty.class);
+            startActivity(i);
+        });
 
     }
 }
